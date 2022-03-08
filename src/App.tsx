dd127c523a09d497df871a0b03790f9790d854a2
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-03-08 22:58:45 +0100

    Install and setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import './App.css'
 import AdviceCard from '@/components/AdviceCard'
+import queryClient from '@/lib/react-query'
 
 function App() {
   return (
     <main className="App">
-      <AdviceCard />
+      <QueryClientProvider client={queryClient}>
+        <AdviceCard />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </main>
   )
 }
