244b081c42292110fb76d25be56f54874553b539
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-03-09 16:58:19 +0100

    Cache fetched advice in localStorage

--- src/hooks/useAdvice.ts
+++ src/hooks/useAdvice.ts
@@ -7,6 +7,16 @@ function useAdvice() {
   return useQuery<Advice, Error>('advice', {
     queryFn: getAdvice,
     staleTime: Infinity, // Advice is never stale (i.e. never refetch unless done by the user)
+    initialData: () => {
+      const currentAdvice = window.localStorage.getItem('currentAdvice')
+
+      if (currentAdvice) {
+        return JSON.parse(currentAdvice) as Advice
+      }
+    },
+    onSuccess: advice => {
+      window.localStorage.setItem('currentAdvice', JSON.stringify(advice))
+    },
     onError: err => alert(`Error encountered: ${err.message}`),
   })
 }
